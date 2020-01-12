b35c1b7fd6728381a2e5c48318f66b7e6db2ba3a
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-01-11 18:41:25 -0800

    added password hashing

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name : {
         type: String,
         required: true,
@@ -34,6 +35,7 @@ const User = mongoose.model('User', {
     email: {
         required: true,
         type: String,
+        unique: true,
         trim: true,
         lowercase: true,
         validate : {
@@ -45,5 +47,37 @@ const User = mongoose.model('User', {
         }
     }
 });
+
+//when throwing errors for authentication it is best to be generic
+//it exposes less information  
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user =  await User.findOne({email}); 
+    
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user; 
+}
+
+//mongoose middleware can only be set up on the user schema.
+//this function runs before the save function.  
+// it checks to see if the password field was modified on the user object
+//if it is modified the new password is hashed.
+userSchema.pre('save', async function (next) {
+    const user = this; 
+
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+const User = mongoose.model('User',userSchema);
 
 module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -48,19 +48,24 @@ router.patch('/users/:id', async (req,res) => {
         return res.status(400).send({error:`not a valid update`});
     }
     try {
-        const usr = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
+
+        const usr = await User.findById(req.params.id);
         
         if(!usr){
             return res.status(404).send();
         }
 
+        potentialUpdates.forEach( (update) => usr[update] = req.body[update]); // add updates to user 
+        await usr.save(); //  validate and save updates
+        
+        //const usr = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
         res.send(usr); 
     } catch (error) {
         res.status(500).send(error);
     }
 });
 
-//delete a user
+//delete and return the user
 router.delete('/users/:id', async(req,res) => {
     const _id = req.params.id;
     
@@ -75,4 +80,15 @@ router.delete('/users/:id', async(req,res) => {
     }
 });
 
+
+// end point for user authentication
+// the method findBycredentials is a user created function that exists on the user model
+router.post('/users/login', async (req,res) => {
+    try {
+        const usr =  await User.findByCredentials(req.body.email,req.body.password);
+        res.send(usr);
+    } catch(e) {
+        res.status(400).send()
+    } 
+})
 module.exports = router; 
