ab7eb708e6e1dab4aac302f6cc29255a1d693474
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-01-14 11:16:26 -0800

    protected update route. added modifications as needed.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -24,8 +24,7 @@ router.get('/users/me', auth,(req,res) => {
 
 
 //edit a user by id
-router.patch('/users/:id', auth, async (req,res) => {
-    const _id = req.params.id;
+router.patch('/users/me', auth, async (req,res) => {
     const potentialUpdates = Object.keys(req.body); 
     const allowedUpdates = ['name', 'email', 'password', 'age'];
 
@@ -35,22 +34,15 @@ router.patch('/users/:id', auth, async (req,res) => {
         return res.status(400).send({error:`Not a valid update.`});
     }
     try {
-
-        const usr = await User.findById(req.params.id);
+        potentialUpdates.forEach( (update) => req.user[update] = req.body[update]); // add updates to user 
+        await req.user.save(); //  validate and save updates
         
-        if(!usr){
-            return res.status(404).send();
-        }
-
-        potentialUpdates.forEach( (update) => usr[update] = req.body[update]); // add updates to user 
-        await usr.save(); //  validate and save updates
-        
-        //const usr = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
-        res.send(usr); 
+        res.send(req.user); 
     } catch (error) {
         res.status(500).send(error);
     }
 });
+        //const usr = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
 
 //delete and return the user
 router.delete('/users/me', auth, async (req,res) => {
