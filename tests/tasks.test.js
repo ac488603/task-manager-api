7f0d642f22ebdda136b2e690a8b2f88d36fc09d3
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-01-31 10:07:03 -0800

    Added new test to task test suite.

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -1,1 +1,19 @@
-test('first test', () => {})
+const request = require('supertest')
+const app = require('../app')
+const Task = require('../models/Task')
+const {userOne, userOneID, setupDatabase} = require('./fixtures/db')
+
+beforeEach(setupDatabase)
+
+test('should create a new task for user', async () => {
+    const response = await request(app).post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description : "write philosphy paper"
+        })
+        .expect(201)
+
+    const task = await Task.findById(response.body._id)
+    expect(task).not.toBeNull();
+    expect(task.completed).toEqual(false)
+})
